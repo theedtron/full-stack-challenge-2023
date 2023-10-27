2d6df50d1328d6cf814341c4435c04ddc0736f55
theed <kareoedwin@gmail.com>
2023-10-27 19:03:20 +0300

    fix home

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/referrals';
 
     /**
      * Create a new controller instance.
